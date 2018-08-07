448108289f4a3ae4ef0648b0e71510560a3e72ee
XanderXu1998 <xanderxu1998@gmail.com>
2018-08-07 21:56:06 +0800

    a

--- DataStructure/readme.md
+++ DataStructure/readme.md
@@ -1,1 +1,7 @@
 ## DataStructure
+### seg_tree_range 区间更新线段树
+sum是答案数组，add是lazy标志，默认是区间求和
+
+若更改功能，需要修改每个函数
+
+
--- DataStructure/seg_tree_range.cpp
+++ DataStructure/seg_tree_range.cpp
@@ -1,28 +1,30 @@
+// 区间求和
+
 // 线段树区间更新
 #define LSON l, m, rt << 1
 #define RSON m + 1, r, rt << 1 | 1
 #define lson rt << 1
 #define rson rt << 1 | 1
-ll sum[N << 2], add[N << 2];
-// 更新答案
-inline void Maintain(const int &rt) {
-    sum[rt] = sum[lson] + sum[rson];
+ll ans[N << 2], lazy[N << 2];
+// 更新答案，需要修改
+inline void Maintain(const int &rt) { 
+    ans[rt] = ans[lson] + ans[rson];
     return;
 }
-// 延时标记下传
+// 延时标记下传，需要修改
 inline void Pushdown(const int &rt, const int &m) {
-    if (add[rt] != 0) { // 倍数变化 add[rt] != 1
-        add[rt << 1] += add[rt];
-        add[rt << 1 | 1] += add[rt];
-        sum[rt << 1] += add[rt] * (m - (m >> 1));
-        sum[rt << 1 | 1] += add[rt] * (m >> 1);
-        add[rt] = 0;
+    if (lazy[rt] != 0) { // lazy更新，需要修改
+        lazy[rt << 1] += lazy[rt];
+        lazy[rt << 1 | 1] += lazy[rt];
+        ans[rt << 1] += lazy[rt] * (m - (m >> 1));
+        ans[rt << 1 | 1] += lazy[rt] * (m >> 1);
+        lazy[rt] = 0;
     }
 }
 void build(int l, int r, const int &rt = 1) {
-    add[rt] = 0; // 倍数变化更改为1
+    lazy[rt] = 0; // lazy数组初值为0
     if (l == r) {
-        sum[rt] = a[rk[l]];
+        ans[rt] = a[rk[l]];  // 线段树初始值，需要修改
         return;
     }
     int m = (l + r) >> 1;
@@ -32,9 +34,9 @@ void build(int l, int r, const int &rt = 1) {
     return;
 }
 void update(int L, int R, ll c, int l, int r, int rt = 1) {
-    if (L <= l && r <= R) {
-        add[rt] += c;
-        sum[rt] += c * (r - l + 1);
+    if (L <= l && r <= R) {  // 区间更新，需要修改
+        lazy[rt] += c;
+        ans[rt] += c * (r - l + 1);
         return;
     }
     Pushdown(rt, r - l + 1);
@@ -45,13 +47,76 @@ void update(int L, int R, ll c, int l, int r, int rt = 1) {
     return;
 }
 ll query(const int &L,const int &R, int l, int r,const int &rt = 1) {
-    if (L <= l && r <= R) {
-        return sum[rt];
+    if (L <= l && r <= R) { 
+        return ans[rt];
     }
     Pushdown(rt, r - l + 1);
     int m = (l + r) >> 1;
-    ll ret = 0;
+    ll ret = 0;  //  统计答案，需要修改
     if (L <= m) ret += query(L, R, LSON);
     if (m < R) ret += query(L, R, RSON);
     return ret;
 }
+
+
+// 区间最值
+
+
+// 线段树区间更新
+#define LSON l, m, rt << 1
+#define RSON m + 1, r, rt << 1 | 1
+#define lson rt << 1
+#define rson rt << 1 | 1
+ll ans[N << 2], lazy[N << 2];
+// 更新答案，需要修改
+inline void Maintain(const int &rt) { 
+    ans[rt] = max(ans[lson] , ans[rson]);
+    return;
+}
+// 延时标记下传，需要修改
+inline void Pushdown(const int &rt, const int &m) {
+    if (lazy[rt] != 0) { // lazy更新，需要修改
+        lazy[rt << 1] += lazy[rt];
+        lazy[rt << 1 | 1] += lazy[rt];
+        ans[rt << 1] += lazy[rt];
+        ans[rt << 1 | 1] += lazy[rt];
+        lazy[rt] = 0;
+    }
+}
+void build(int l, int r, const int &rt = 1) {
+    lazy[rt] = 0; // lazy数组初值为0
+    if (l == r) {
+        ans[rt] = a[l];  // 线段树初始值，需要修改
+        return;
+    }
+    int m = (l + r) >> 1;
+    build(LSON);
+    build(RSON);
+    Maintain(rt);
+    return;
+}
+void update(int L, int R, ll c, int l, int r, int rt = 1) {
+    if (L <= l && r <= R) {  // 区间更新，需要修改
+        lazy[rt] += c;
+        ans[rt] += c;
+        return;
+    }
+    Pushdown(rt, r - l + 1);
+    int m = (l + r) >> 1;
+    if (L <= m) update(L, R, c, LSON);
+    if (m < R) update(L, R, c, RSON);
+    Maintain(rt);
+    return;
+}
+ll query(const int &L,const int &R, int l, int r,const int &rt = 1) {
+    if (L <= l && r <= R) { 
+        return ans[rt];
+    }
+    Pushdown(rt, r - l + 1);
+    int m = (l + r) >> 1;
+    ll ret = 0;  //  统计答案，需要修改
+    if (L <= m) ret = max(ret,query(L, R, LSON));
+    if (m < R)  ret = max(ret,query(L, R, RSON));
+    return ret;
+}
+
