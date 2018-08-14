929a88b5051067a6930da66d57fed2c23e766377
Bocity <haolovej@vip.qq.com>
2018-08-14 10:56:24 +0800

    fwt递归版本

--- Math/FWT.cpp
+++ Math/FWT.cpp
@@ -26,3 +26,29 @@ void FWT_xor(int *a, int opt) {
                 if (opt == -1) a[j + k] = 1ll * a[j + k] * inv2 % MOD, a[i + j + k] = 1ll * a[i + j + k] * inv2 % MOD;
             }
 }
+void fwt(ll a[], int l, int r) {
+    if (l == r) return;
+    int mid = (l + r) >> 1;
+    fwt(a, l, mid);
+    fwt(a, mid + 1, r);
+    int len = mid - l + 1;
+    for (int i = l; i <= mid; ++i) {
+        ll x1 = a[i];
+        ll x2 = a[i + len];
+        a[i] = x1 + x2;
+        a[i + len] = x1 - x2;
+    }
+}
+void ifwt(ll a[], int l, int r) {
+    if (l == r) return;
+    int mid = (l + r) >> 1;
+    int len = mid - l + 1;
+    for (int i = l; i <= mid; ++i) {
+        ll y1 = a[i];
+        ll y2 = a[i + len];
+        a[i] = (y1 + y2) / 2;
+        a[i + len] = (y1 - y2) / 2;
+    }
+    ifwt(a, l, mid);
+    ifwt(a, mid + 1, r);
+}
