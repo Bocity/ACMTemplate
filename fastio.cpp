5df845448f7506a0fa4bc66e0e22e64eb406540e
Bocity <haolovej@vip.qq.com>
2018-08-24 12:22:08 +0800

    fuck xx

--- fastio.cpp
+++ fastio.cpp
@@ -1,5 +1,6 @@
 // maxlen 输出单位长度（只影响速度，不用改）
 // xchar/wchar 读入/输出字符，基本函数
+// xuint/wuint，读入/输出无符号整数(ull)
 // xint/wint，读入/输出符号整数(ll)
 // xstring/wstring，读入/输出char字符串
 
@@ -15,29 +16,27 @@ struct FastIO {
         static char buf[S];
         static int len = 0, pos = 0;
         if (pos == len) pos = 0, len = fread(buf, 1, S, stdin);
-        if (pos == len) return 0;
+        if (pos == len) exit(0);
         return buf[pos++];
     }
-    inline bool xint(auto &x) {
-        int s = 1, c = xchar();x = 0;
-        while (c && c <= 32) c = xchar();
+    inline int xint() {
+        int s = 1, c = xchar();int x = 0;
+        while (c <= 32) c = xchar();
         if (c == '-') s = -1, c = xchar();
         for (; '0' <= c && c <= '9'; c = xchar()) x = x * 10 + c - '0';
-        x*=s;
-        return c;
+        return x * s;
     }
-    inline bool xstring(char *s) {
+    inline void xstring(char *s) {
         int c = xchar();
-        while (c && c <= 32) c = xchar();
+        while (c <= 32) c = xchar();
         for (; c > 32; c = xchar()) *s++ = c;
         *s = 0;
-        return c;
     }
     inline void wchar(int x) {
         if (wpos == S) fwrite(wbuf, 1, S, stdout), wpos = 0;
         wbuf[wpos++] = x;
     }
-    inline void wint(auto x) {
+    inline void wint(int x) {
         if (x < 0) wchar('-'), x = -x;
         char s[24];
         int n = 0;
@@ -47,10 +46,9 @@ struct FastIO {
     inline void wstring(const char *s) {
         while (*s) wchar(*s++);
     }
-    inline void wln() {
-        wchar('\n');
-    }
     ~FastIO() {
         if (wpos) fwrite(wbuf, 1, wpos, stdout), wpos = 0;
     }
 } io;
+
+
